8bfe204b7bf204d67e70260589680c9bdacf9485
Tiago silva <163779808+ttiagosilv@users.noreply.github.com>
2024-12-11 12:15:59 -0300

    Delete sem-10 directory

--- sem-10/Sem-10-T1-Q1.py
+++
@@ -1,5 +0,0 @@
-def mostrar_numeros():
-    for i in range(0, 50):
-        print(i+1)
-
-mostrar_numeros()
--- sem-10/Sem-10-T1-Q4.py
+++
@@ -1,8 +0,0 @@
-def gerar_sequencia():
-    sequencia = ""
-    for i in range(10, 1001, 10):
-        sequencia += str(i) + ", "
-    sequencia = sequencia[:-2] + "."
-    return sequencia
-
-print(gerar_sequencia())
